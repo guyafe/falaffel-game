1ce7d995920637a7f41a7b9ea93210d4dc8b0972
Guy Yafe <guyy@elisra.com>
2021-02-07 21:53:27 +0200

    Thank the fish

--- src/guy/App.java
+++ src/guy/App.java
@@ -2,7 +2,7 @@ package guy;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Dear World!");
+        System.out.println("Goodbye, Dear World!  and thanks for all the fish");
         System.out.println("I love Moran");
     }
 }
