dc60c16de52408e115ff3c5285dbb5c52aebd254
Tal Yafe <talyafe85@gmail.com>
2021-02-07 21:43:00 +0200

    add  another massage

--- src/guy/App.java
+++ src/guy/App.java
@@ -3,5 +3,6 @@ package guy;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Dear World!");
+        System.out.println("I love Moran");
     }
 }
