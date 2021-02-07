c8873401996a19667e78218e1206a9192efa8906
Guy Yafe <guyy@elisra.com>
2021-02-07 21:00:55 +0200

    Change Message

--- src/guy/App.java
+++ src/guy/App.java
@@ -2,6 +2,6 @@ package guy;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Dear World!");
     }
 }
