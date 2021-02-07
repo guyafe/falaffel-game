e8093428b195a918c928557d7f3114a7d60fa54c
Tal Yafe <talyafe85@gmail.com>
2021-02-07 21:50:12 +0200

    very dear

--- src/guy/App.java
+++ src/guy/App.java
@@ -2,7 +2,7 @@ package guy;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Dear World!");
+        System.out.println("Hello, Very Dear World!");
         System.out.println("I love Moran");
     }
 }
